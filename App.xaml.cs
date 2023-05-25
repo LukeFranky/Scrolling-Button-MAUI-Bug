dfdcf7f9f902fc3ca17d59a3ede8baa286ce19ca
Luke Franklin <luke@practicalsystems.com.au>
2023-05-26 08:26:55 +1000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Scrolling_Button_MAUI_Bug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Scrolling_Button_MAUI_Bug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
